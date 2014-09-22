28c31c41b27613da95e376ef65b5eb15a7274960
Daniel Harms <jdharms@gmail.com>
2014-09-21 17:28:15 -0700

    Adds user registration capability
    Form, view and template for registration page.

--- app/auth/forms.py
+++ app/auth/forms.py
@@ -1,9 +1,24 @@
 from flask.ext.wtf import Form
-from wtforms import StringField, PasswordField, BooleanField, SubmitField
-from wtforms.validators import Required
+from wtforms import StringField, PasswordField, BooleanField, SubmitField, ValidationError
+from wtforms.validators import Required, Length, Regexp, EqualTo
+from ..models import User
 
 class LoginForm(Form):
     username = StringField('Username', validators=[Required()])
     password = PasswordField('Password', validators=[Required()])
     remember_me = BooleanField('Keep me logged in')
     submit = SubmitField('Log In')
+
+
+class RegistrationForm(Form):
+    username = StringField('Username', validators=[Required(), Length(1, 64), Regexp('^[a-zA-Z0-9][a-zA-Z0-0_.]*$', 0,
+                                                    'Usernames must consist only of letters, numbers, underscores and periods. '
+                                                    'They must begin with a letter or number.')])
+    password = PasswordField('Password', validators=[Required(), EqualTo('password2', message='Passwords must match')])
+    password2 = PasswordField('Confirm Password', validators=[Required()])
+    submit = SubmitField('Register')
+
+    def validate_username(self, field):
+        if User.query.filter_by(username=field.data).first():
+            raise ValidationError('Username already in use!')
+
--- app/auth/views.py
+++ app/auth/views.py
@@ -1,8 +1,9 @@
 from . import auth
-from .forms import LoginForm
+from .forms import LoginForm, RegistrationForm
 from flask import render_template, redirect, request, url_for, flash
 from ..models import User
 from flask.ext.login import login_user, login_required, logout_user
+from .. import db
 
 @auth.route('/login', methods=['GET', 'POST'])
 def login():
@@ -21,3 +22,14 @@ def logout():
     logout_user()
     flash('You have been logged out')
     return redirect(url_for('main.index'))
+
+@auth.route('/register', methods=['GET', 'POST'])
+def register():
+    form =  RegistrationForm()
+    if form.validate_on_submit():
+        user = User(username=form.username.data, password=form.password.data)
+        db.session.add(user)
+        db.session.commit()
+        flash('Registration successful!')
+        return redirect(url_for('main.index'))
+    return render_template('auth/register.html', registration_form=form)
