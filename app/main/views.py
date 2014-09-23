eb0485414e0c2354317bc95f9b8eddd9977e58a3
Daniel Harms <jdharms@gmail.com>
2014-09-22 23:41:29 -0700

    Very basic user profile pages.

--- app/main/views.py
+++ app/main/views.py
@@ -1,4 +1,4 @@
-from flask import render_template, session, url_for, redirect
+from flask import render_template, session, url_for, redirect, abort
 from . import main
 from .. import db
 from ..models import User
@@ -6,3 +6,10 @@ from ..models import User
 @main.route('/', methods=['GET', 'POST'])
 def index():
     return render_template('index.html')
+
+@main.route('/user/<username>')
+def user(username):
+    user = User.query.filter_by(username=username).first()
+    if user is None:
+        abort(404)
+    return render_template('user.html', user=user)
